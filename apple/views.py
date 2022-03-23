b6928bdbfd2ff17bd8dab18ae753e0bc9966c669
hwangstone1 <mylee553@naver.com>
2022-03-23 23:54:19 +0900

    -

--- apple/views.py
+++ apple/views.py
@@ -5,7 +5,8 @@ from board.models import Question
 
 def main(request):
 
-    question_list = Question.objects.order_by('-create_date')
+    question_list = Question.objects.order_by('-create_date')[:5]
+
 
 
     return render(request, 'apple/apple.html', {'question_list' : question_list})
